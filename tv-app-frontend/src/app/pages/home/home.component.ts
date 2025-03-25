46306727f86e20a09644abb34f7ed6857e4a9e10
Jewel Josef P. Seno <seno.jeweljosef@gmail.com>
2025-03-26 03:13:26 +0800

    Update routing

--- tv-app-frontend/src/app/pages/home/home.component.ts
+++ tv-app-frontend/src/app/pages/home/home.component.ts
@@ -10,19 +10,28 @@ import { PlayComponent } from './components/play/play.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  title = 'Bee Movie'
-  description = `Barry B. Benson, an idealistic honey bee who has the ability to talk to humans, has recently graduated from college and is about to enter the hive's honey-making workforce, Honex Industries, with his best friend, Adam Flayman. Barry is initially excited, but his ambitious, insubordinate attitude emerges upon discovering that his choice of job will never change once picked. Later, the two bees run into a group of pollen jocks, bees who collect pollen from flowers outside the hive, and they offer to take Barry with them if he is "bee enough". While on his first pollen-gathering expedition in New York City, Barry gets lost in the rain, and ends up on the windowsill of a human florist named Vanessa Bloome. Upon noticing Barry, Vanessa's boyfriend Ken attempts to squash him, but Vanessa gently catches and releases Barry outside, saving his life.`
-  videoId = 3
+  ////
+  // This is actually watch.component logic
+  ///
+  videoId = 33
   hasLoaded = signal(false)
   error = signal<string | null>(null)
+  title = signal<string | null>(null)
+  description = signal<string | null>(null)
   
   ngOnInit(): void {
     console.log("initial loaded val: ", this.hasLoaded())
-    axios.get('https://fakestoreapi.com/products')
+    axios.get(`https://fakestoreapi.com/products/${this.videoId}`)
       .then(response => {
         console.log(response.data)
-        this.hasLoaded.set(true)
-        console.log("loaded val: ", this.hasLoaded())
+        if (response.data){
+          this.hasLoaded.set(true)
+          this.title.set(response.data.title)
+          this.description.set(response.data.description)
+          console.log("loaded val: ", this.hasLoaded())
+        } else {
+          this.error.set('Empty Response');
+        }
       })
       .catch(error => {
         console.error('API Error:', error)
